a1b2d3eafe1c7a889ba8016f1ae3d9812fb766a2
sohanemon <m.sohanemon@gmail.com>
2023-05-08 20:47:33 +0600

    effects added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
+'use client';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Image from 'next/image';
 import hill from '../assets/hill.png';
 import sky from '../assets/sky.jpg';
 
 export default function Home() {
+  const { scrollY } = useScroll();
+
+  const y = useTransform(scrollY, [0, 5000], ['0%', '100%']);
+  const sunY = useTransform(scrollY, [0, 500], ['0%', '500%']);
+  const sunX = useTransform(scrollY, [0, 500], ['0%', '-500%']);
+  const sunColor = useTransform(scrollY, [0, 500], ['#ff0', '#f00']);
+
   return (
     <main className='relative'>
       <div
         style={{ backgroundImage: `url(${sky.src})` }}
         className='w-full h-screen bg-bottom bg-cover '
       >
-        <Image src={hill} alt='hill' className='absolute -bottom-1/2' />
+        <motion.div
+          style={{ y: sunY, x: sunX, backgroundColor: sunColor }}
+          className='absolute w-32 rounded-full right-10 top-10 aspect-square'
+        />
+        <motion.div style={{ y }} className='absolute -bottom-1/2'>
+          <Image src={hill} alt='hill' />
+        </motion.div>
       </div>
     </main>
   );
